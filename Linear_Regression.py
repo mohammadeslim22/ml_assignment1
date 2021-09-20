df7d88bcd43c5bb9009ed7e795d68dff9d843d11
mohammadeslim22 <mohammadeslim@gmail.com>
2021-09-20 11:01:46 -0600

    linear regression line

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -16,13 +16,16 @@ def AcitvateLinearRegression():
     y_pred = regressor.predict(Feature_Engineering.x_test)
     eval_df = pd.DataFrame({'Actual': Feature_Engineering.y_test, 'Predicted': y_pred})
     print(eval_df)
+
     print(type(Feature_Engineering.x_train['depature_midnight']))
     print(type(Feature_Engineering.y_train))
     print(Feature_Engineering.x_train.head(3))
     print("_______________________________")
     print(Feature_Engineering.y_train.head(3))
     plt.scatter(Feature_Engineering.x_train[0], Feature_Engineering.y_train)
-    plt.plot(Feature_Engineering.x_test, y_pred, color='red')
+    # m, b = np.polyfit(Feature_Engineering.x_test[0], y_pred, 1)
+    # plt.plot(Feature_Engineering.x_test[0], m * Feature_Engineering.x_test[0] + b)
+    plt.plot(Feature_Engineering.x_test[0], y_pred, color='red')
     plt.show()
 
     print('Mean Absolute Error:', metrics.mean_absolute_error(Feature_Engineering.y_test, y_pred))
