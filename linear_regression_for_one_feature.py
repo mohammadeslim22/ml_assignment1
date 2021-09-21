a040554039fda91556b609a53e46f9f012221f63
mohammadeslim22 <mohammadeslim@gmail.com>
2021-09-21 08:09:47 -0600

    linear regression for one feature

---
+++ linear_regression_for_one_feature.py
@@ -0,0 +1,35 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import Feature_Engineering
+from sklearn import metrics
+import numpy as np
+import sklearn
+
+
+y_test=Feature_Engineering.test['Delay']/60
+x_test=Feature_Engineering.test[['flight duration']]
+y_train=Feature_Engineering.train['Delay']/60
+x_train=Feature_Engineering.train[['flight duration']]
+
+x_train=np.asarray(x_train).astype(np.float32)
+y_train=np.asarray(y_train).astype(np.float32)
+x_test=np.asarray(x_test).astype(np.float32)
+y_test=np.asarray(y_test).astype(np.float32)
+
+x_train=x_train.reshape(-1, 1)
+x_test=x_test.reshape(-1,1)
+y_train=y_train.reshape(-1, 1)
+y_test=y_test.reshape(-1,1)
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+print(f"Model intercept : {regressor.intercept_}")
+print(f"Model coefficients : {regressor.coef_}")
+
+y_pred = regressor.predict(x_test)
+
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(Feature_Engineering.y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(Feature_Engineering.y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(Feature_Engineering.y_test, y_pred)))
+sklearn.metrics.r2_score(Feature_Engineering.y_test, y_pred, sample_weight=None, multioutput='uniform_average')
--- nural_network_test.py
+++ nural_network_test.py
@@ -8,11 +8,11 @@ from sklearn import metrics
 print(Feature_Engineering.x_train.shape)
 def ActivateNeuralNetwork():
     model2 = Sequential()
-    model2.add(Dense(20, input_dim=37, activation='relu'))
-    model2.add(Dense(10, activation='relu'))
-    # model2.add(Dense(128, input_dim=37, activation='relu'))
-    # model2.add(Dense(64, activation='relu'))
-    # model2.add(Dense(32, activation='relu'))
+    # model2.add(Dense(20, input_dim=37, activation='relu'))
+    # model2.add(Dense(10, activation='relu'))
+    model2.add(Dense(128, input_dim=37, activation='relu'))
+    model2.add(Dense(64, activation='relu'))
+    model2.add(Dense(32, activation='relu'))
     model2.add(Dense(1, activation='relu'))
     model2.compile(loss='mean_absolute_error',
                    optimizer='adam',
